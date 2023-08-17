ae2ce608e204111ca915d3b98dd4c3bcc9064b26
Junn Eric Timoteo <juntyms@yahoo.com>
2023-08-17 14:22:40 +0400

    added Middleware alias for New Middleware - Admin Middleware - User Middleware

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'adminOnly' => \App\Http\Middleware\EnsureTokenForAdmin::class,
+        'userOnly' => \App\Http\Middleware\EnsureTokenForUser::class,
     ];
 }
