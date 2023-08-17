7ac943626ca25cda56c507e0afc85f5d307cb0af
Junn Eric Timoteo <juntyms@yahoo.com>
2023-08-17 14:36:43 +0400

    AdminsController implementation and swagger api documentation

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -5,7 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+use OpenApi\Annotations as OA;
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Petshop Ecommerce",
+ *      description="Petshop Ecommerce OpenApi Swagger Documentation",
+ *      @OA\Contact(
+ *          email="juntyms@gmail.com"
+ *      ),
+ *     @OA\License(
+ *         name="MIT",
+ *         url="https://opensource.org/licenses/MIT"
+ *     ),
+ *  ),
+ *  @OA\SecurityScheme(
+ *       type="http",
+ *       description="Authorization with JWT",
+ *       name="Authorization",
+ *       in="header",
+ *       scheme="bearer",
+ *       bearerFormat="JWT",
+ *       securityScheme="bearerToken"
+ *  )
+ *
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
