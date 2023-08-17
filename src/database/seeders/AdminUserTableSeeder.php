5237272f55dc3a062351ae73fb7861d44c404b5b
Junn Eric Timoteo <juntyms@yahoo.com>
2023-08-17 14:27:12 +0400

    Updated UserFactory, added new AdminUser Seeder and updated DatabaseSeeder to be able to seed AdminUser Seeder File

---
+++ src/database/seeders/AdminUserTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \DB::table('users')->delete();
+
+        User::factory([
+            'first_name'=> 'Admin',
+            'last_name'=> 'Admin',
+            'is_admin'=>1,
+            'email'=> 'admin@buckhill.co.uk',
+            'password'=> Hash::make('userpassword')
+        ])->create();
+    }
+}
