b14dc448d9c25d3fc4298ac908cd427b4e8c97ca
Junn Eric Timoteo <juntyms@yahoo.com>
2023-08-18 22:14:04 +0400

    renamed AdminStoreRequest to UserStoreRequest

--- src/app/Http/Requests/Api/V1/UserStoreRequest.php
+++ src/app/Http/Requests/Api/V1/UserStoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Api\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AdminStoreRequest extends FormRequest
+class UserStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,10 +22,10 @@ class AdminStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'first_name'=> 'required',
+            'first_name' => 'required',
             'last_name' => 'required',
             'email' => 'required|unique:users',
-            'password' =>'required',
+            'password' => 'required',
             'address' => 'required',
             'phone_number' => 'required'
         ];
