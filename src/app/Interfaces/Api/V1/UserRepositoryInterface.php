49174533c100d28aa6540948036dd31a539aa1d4
Junn Eric Timoteo <juntyms@yahoo.com>
2023-08-18 22:15:25 +0400

    created UserRepository Interface to be used for the UserController to implement Repository Pattern

--- src/app/Interfaces/Api/V1/UserRepositoryInterface.php
+++ src/app/Interfaces/Api/V1/UserRepositoryInterface.php
@@ -4,8 +4,9 @@ namespace App\Interfaces\Api\V1;
 
 interface UserRepositoryInterface
 {
-    public function getAllUserOrders($userId);
+    public function createUser(array $userDetails);
     public function getUserDetails($userId);
     public function updateUserDetails($userId, array $newUserDetails);
     public function deleteUser($userId);
+    public function getAllUserOrders($userId);
 }
