8415861d28ba9589dd14e83292b21ceee53012b9
Junn Eric Timoteo <juntyms@yahoo.com>
2023-08-19 23:29:55 +0400

    added OrderResource

--- src/app/Http/Resources/Api/V1/UserResource.php
+++ src/app/Http/Resources/Api/V1/UserResource.php
@@ -15,7 +15,6 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
 
-
         return [
                 'id' => (string)$this->id,
                 'type' => 'User',
