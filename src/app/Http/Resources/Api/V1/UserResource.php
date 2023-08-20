8c82857d7f5ad032e6ff943637d0b93d773c6b2e
Junn Eric Timoteo <juntyms@yahoo.com>
2023-08-20 13:43:00 +0400

    added email in resource

--- src/app/Http/Resources/Api/V1/UserResource.php
+++ src/app/Http/Resources/Api/V1/UserResource.php
@@ -22,6 +22,7 @@ class UserResource extends JsonResource
                     'uuid' => (string)$this->uuid,
                     'first_name' => $this->first_name,
                     'last_name' => $this->last_name,
+                    'email' => $this->email,
                     'address' => $this->address,
                     'phone_number' => $this->phone_number,
                     'is_admin' => $this->is_admin ? true : false,
