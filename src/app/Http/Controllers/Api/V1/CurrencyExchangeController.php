401fe7ffa7b601e3b68ac0b6cc239bfb81b70a57
Junn Eric Timoteo <juntyms@yahoo.com>
2023-08-22 13:50:45 +0400

    created new currency converter endpoint add new CurrencyExchangeController

---
+++ src/app/Http/Controllers/Api/V1/CurrencyExchangeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use CurrencyExchange;
+use Illuminate\Http\Request;
+
+class CurrencyExchangeController extends Controller
+{
+    public function convertCurrency($currency, $amount)
+    {
+        $conversion = CurrencyExchange::currency($currency)
+            ->amount($amount)
+            ->getConversion();
+
+        return response()->json([
+            'Amount' => $conversion
+        ]);
+    }
+}
