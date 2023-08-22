984daef03da5ac1e5c7ec0f16bdb098cc6dc1db9
Junn Eric Timoteo <juntyms@yahoo.com>
2023-08-22 13:17:02 +0400

    added Package service provider

---
+++ src/packages/juntyms/currencyexchange/src/CurrencyExchangeServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Juntyms\CurrencyExchange;
+
+use Illuminate\Support\ServiceProvider;
+
+class CurrencyExchangeServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
