7d7d3071571fdc48b09393cd1cf39ac68f403b43
Junn Eric Timoteo <juntyms@yahoo.com>
2023-08-22 22:32:47 +0400

    new test

---
+++ src/packages/juntyms/currencyexchange/tests/Feature/CurrencyExchangeTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Juntyms\CurrencyExchange\Tests;
+
+use Juntyms\CurrencyExchange\CurrencyParser;
+use Juntyms\CurrencyExchange\Facades\CurrencyExchange;
+
+class CurrencyExchangeTest extends TestCase
+{
+    /** @test */
+    public function parser_can_parse_uri()
+    {
+        $decoded = CurrencyParser::getXML();
+
+        $this->assertEquals(count($decoded['Cube']['Cube']['Cube'], COUNT_NORMAL), 30);
+    }
+
+    /** @test */
+    public function can_return_the_conversion_value()
+    {
+        $value = CurrencyExchange::currency('PHP')
+            ->amount(100)
+            ->getConversion();
+
+
+        $this->assertEquals(round($value, 2), 6125.10);
+
+    }
+}
